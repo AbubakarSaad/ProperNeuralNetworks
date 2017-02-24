efc0507f2ef82b7b48b7f5dd9b421506dc1514f7
Abu <abubakar.saad.891@gmail.com>
2017-02-24 01:59:18 -0500

    Added hold-out and part of k-fold

--- neuralnetwork/__main__.py
+++ neuralnetwork/__main__.py
@@ -9,45 +9,60 @@ from functions import Functions
 
 # validation
 # k-fold
-# hold out (70, 30)
+
+
+
 
 def main():
 
     learningRate = 0.3
     momentum = 0.1
-    epoch = 100
+    epoch = 1
     weightconnectionstoH = 64
     numberofHiddenNeuron = 40
     numberofOutputNeuron = 10 
     sizeofBaish = numberofHiddenNeuron
     sizeofBaiso = numberofOutputNeuron
+    k = 10
 
     # opening a file
     # os.path.dirname gets the dir path for this file
-    filepath = "c:\\Users\\Abu\\Documents\\ANN\\assignment1\\neuralnetwork\\a1digits\\"
-    dirlisting = os.listdir(filepath)
-    # print(dirlisting)
+    filepath = "/Users/abubakarsaad/Documents/cosc4p80/ProperNeuralNetworks/neuralnetwork/a1digits/"
+    dirlistingtraining = os.listdir(filepath)
+    # print(dirlistingtraining)
 
     # 0 - 9 are the test files and 10 - 19 are the training file
     # np.random.seed(100)
 
-    # path = filepath + dirlisting[random_file_num]
+    # path = filepath + dirlistingtraining[random_file_num]
     # inputs = np.loadtxt(path, delimiter=',', dtype='float')
-    inputs = []
-    # print(inputs[0])
-    for i in range(10, len(dirlisting)): 
-        path = filepath + dirlisting[i]
+    inputsTraining = []
+    
+    # print(inputsTraining[0])
+    for i in range(10, len(dirlistingtraining)): 
+        path = filepath + dirlistingtraining[i]
         inp = np.loadtxt(path, delimiter=',', dtype='float')
         for j in range(len(inp)):
-            inputs.append(inp[j])
+            inputsTraining.append(inp[j])
 
     # k-fold cross validation
 
-    data = inputs
-    track = [i for i in range(0, len(inputs))]
-    input_data = list(zip(data, track))
-
+    # hold out
+    inputsTesting = []
+    for i in range(0, len(dirlistingtraining) - 10): 
+        path = filepath + dirlistingtraining[i]
+        inp = np.loadtxt(path, delimiter=',', dtype='float')
+        for j in range(len(inp)):
+            inputsTesting.append(inp[j])
     
+    trackTesting = [i for i in range(0, len(inputsTesting))]
+    
+    data = inputsTraining
+    track = [i for i in range(0, len(inputsTraining))]
+    input_data = list(zip(data, track))
+    input_testing = list(zip(inputsTraining, trackTesting))
+    
+    # number of weights connections from inputs, number of hidden neurons, output weight connections and number of output neuron
     weightsforh = np.random.uniform(-0.5, 0.5, size=(weightconnectionstoH, numberofHiddenNeuron))
     weightsforo = np.random.uniform(-0.5, 0.5, size=(numberofHiddenNeuron, numberofOutputNeuron))
 
@@ -61,9 +76,20 @@ def main():
         print('\nEpoch number: ', fe)
         np.random.shuffle(input_data)
         data, track = zip(*input_data)
-        # number of weights connections from inputs, number of hidden neurons, output weight connections and number of output neuron
+        
+        test_data =np.split(np.asarray(data), k)
+        test_track = np.split(np.asarray(track), k)
+        print('Chucks: ', len(test_track), len(test_track))
+
+        # for i in range(k):
+        #     for j in range(k-1):
+        #     # go through training
+        #     for z in range(1):
+        #     # collect the aveage 
+
+
         accuracy = 0
-        for e in range(len(data)):
+        for e in range(len(data) - 6999):
             trackId = track[e]
             feedfor.feedforward(data[e])
             feedforwardoutput = feedfor.getOutputofOutputLayer()
@@ -85,10 +111,22 @@ def main():
             if maxValue > 0.5000000000000: 
                 if digit == excepted:
                     accuracy += 1
+        print("Training accuracy of the system: ", (accuracy/len(data)), "%", accuracy)
 
-        # print("Error in the system: ", 0.5*(np.sum((err) ** 2)))
-        # 
-        print("accuracy of the system: ", (accuracy/len(data)), "%", accuracy)
+        if fe == epoch-1:
+            np.random.shuffle(input_testing)
+            inputsTesting, trackTesting = zip(*input_testing)
+            accuracyTesting = 0
+            for t in range(len(inputsTesting)):
+                feedfor.feedforward(inputsTesting[t])
+                feedforwardoutput = feedfor.getOutputofOutputLayer()
+                maxValue = np.amax(feedforwardoutput)
+                digit = list(feedforwardoutput).index(maxValue)
+                excepted = list(Functions().getIncdices(trackTesting[t])).index(1)
+                if maxValue > 0.5000000000000: 
+                    if digit == excepted:
+                        accuracyTesting += 1
+            print("Testing accuracy of the system: ", (accuracyTesting/len(inputsTesting)), "%", accuracyTesting)
         
 
 
